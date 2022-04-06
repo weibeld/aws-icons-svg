500b6142147398f819af061c7bf369915679dfc8
Daniel Weibel <danielmweibel@gmail.com>
2022-04-06 14:52:07 -0500

    Remove old icon set, replace old build script, and adapt README

--- README.md
+++ README.md
@@ -1,40 +1,20 @@
-# AWS Architecture Icons
+# AWS Icons SVG
 
-AWS architecture icons in SVG format.
+The official AWS icon set in SVG format.
 
 ## Description
 
-This repository contains the SVG versions of all the icons from the official [AWS architecture icon assets package](https://aws.amazon.com/architecture/icons/).
+This repository contains the SVG versions of the official [AWS icons set](https://aws.amazon.com/architecture/icons/). The content is generated with the [`build.sh`](build.sh) script.
 
-The content is generated from the AWS architecture icons assets package with the `build.sh` script.
+## Usage
 
-## Icons
-
-**[Services](Asset-Package_20210131/Architecture-Service-Icons_01-31-2021):**
-
-![AWS architecture icons](img/icons-services.png)
-
-**[Resources](Asset-Package_20210131/Resource-Icons_01-31-2021):**
-
-![AWS architecture icons](img/icons-resources.png)
-
-**[Categories](Asset-Package_20210131/Category-Icons_01-31-2021):**
-
-![AWS architecture icons](img/icons-categories.png)
+To find a specific icon, best use the _Go to file_ function of GitHub and start typing the name of the icon.
 
 ## Versions
 
-Current and past versions of the [AWS architecture icons assets package](https://aws.amazon.com/architecture/icons/):
+Current and past versions of icon set.
 
 | Version      | URL |
 |--------------|-----|
+| **01312022** | https://d1.awsstatic.com/webteam/architecture-icons/q1-2022/Asset-Package_01312022.735e45eb7f0891333b7fcce325b0af915fd44766.zip |
 | **20210131** | https://d1.awsstatic.com/webteam/architecture-icons/q1-2021/AWS-Architecture_Asset-Package_20210131.a41ffeeec67743738315c2585f5fdb6f3c31238d.zip |
-
-
-## Generate images
-
-To generate the icon grid images, extract all PNG versions of the icons (instead of SVG) of a specific size with the `build.sh` script, then use the [ImageMagick](https://imagemagick.org/index.php) `montage` command along the following lines:
-
-```bash
-montage -tile 30x0 -geometry +2+2 -scale 80x80 */*.png out.png
-```
--- build.sh
+++ build.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
-#
-# Download an AWS architecture icons assets package and clean it to contain
-# only the SVG versions of the icons in the 48x48 px size.
-#
-# This script makes the following hard assumptions on the structure of the
-# AWS assets package:
-#
-#   - It is a zip file
-#   - The zip file contains a top-level folder
-#   - The top-level folder contains a number of other zip files
-#   - The secondary zip files contain folders and subfolders
-#   - The subfolders that contain the icon files include the size in their
-#     name, e.g., "64", "Arch_64", "Res_48_Dark"
-#   - The size 48 exists for all icon sets, that is, every folder must
-#     eventually contain a subfolder containing "*48*" in its name
-#
-# The above assumption are valid for package version 20210131 [1]. If the
-# format of the package changes in future version, this script must be adapted.
-#
-# The latest version of the AWS architecture icon assets package is published
-# on (select "Asset Package"):
-#
-#    https://aws.amazon.com/architecture/icons/
-#
-# [1] https://d1.awsstatic.com/webteam/architecture-icons/q1-2021/AWS-Architecture_Asset-Package_20210131.a41ffeeec67743738315c2585f5fdb6f3c31238d.zip
 
 set -e
-shopt -u nullglob
 
 usage() {
   cat <<EOF
@@ -34,51 +8,25 @@ USAGE
   $(basename $0) <url>
 
 ARGS
-  <url>  URL of the AWS assets package. The latest version of this package is
-         published on https://aws.amazon.com/architecture/icons/.
+  <url>  AWS asset package (see https://aws.amazon.com/architecture/icons/)
 EOF
 }
 
 [[ "$#" -eq 1 ]] || { usage; exit 1; }
 
 URL=$1
-ROOT=$(pwd)
+ZIP=$(basename "$URL")
+DIR=$(basename $(dirname "$URL"))
 
-# Check whether the supplied directory contains size-specific subdirectories
-is_leaf() {
-  ls -d "$1"/*48*/ &>/dev/null
-}
+# Download and unzip asset package
+mkdir "$DIR"
+wget -q -P "$DIR" "$URL"
+unzip -q -d "$DIR" "$DIR/$ZIP"
+rm "$DIR/$ZIP"
+rm -rf "$DIR/__MACOSX"
 
-# Process a subdirectory: if it's a leaf, copy out all the SVG files of the
-# desired size; else, recursively iterate through all the subdirectories.
-process() {
-  if is_leaf "$1"; then
-    echo "Writing $ROOT/$1..."
-    mkdir -p "$ROOT/$1"
-    if ls "$1"/*48*/*.svg &>/dev/null; then
-      mv "$1"/*48*/*.svg "$ROOT/$1"
-    fi
-  else
-    for d in "$1"/*; do
-      process "$1/$(basename "$d")"
-    done
-  fi
-}
+# Store base name of ZIP file (containing the exact version) in version file
+echo "${ZIP%.zip}" >>"$DIR/version"
 
-# Change into a temporary directory (stay there for the rest of the script)
-TMP=$(mktemp -d)
-cd "$TMP"
-
-# Download and unzip assets package
-wget --quiet -O tmp.zip "$URL"
-unzip -q tmp.zip && rm tmp.zip
-rm -rf __MACOSX
-DIR=$(ls -d */ | head -n 1 | sed 's#/$##')
-
-# Unzip and process each contained zip file
-for z in $DIR/*.zip; do
-  SUBDIR=${z%.*}
-  unzip -q -d "$SUBDIR" "$z" && rm "$z"
-  rm -rf "$SUBDIR/__MACOSX"
-  process "$SUBDIR"
-done
+# Delete all PNG files
+find "$DIR" -name '*.png' -delete
